c154a2c3a35f61e9bf821fa85f3df2e0d4bb30ae
YooHayoung <yoo971202@naver.com>
2023-01-07 11:36:11 +0900

    init

---
+++ src/main/java/com/comeon/backend/ComeOnBackendApplication.java
@@ -0,0 +1,13 @@
+package com.comeon.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeOnBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComeOnBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/comeon/backend/ComeOnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comeon.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComeOnBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
