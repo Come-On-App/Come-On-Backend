347a8d51324dc6336db4cd827fe7fc8dcb0dc6ad
hi0yoo <yoo971202@naver.com>
2023-01-15 11:44:17 +0900

    Fix: AppAuthTokenResponseUtil
    AppAuthTokenResponse 생성과정 RefreshToken Null인 경우 오류 발생 해결

--- src/main/java/com/comeon/backend/user/presentation/api/AppAuthTokenResponseUtil.java
+++ src/main/java/com/comeon/backend/user/presentation/api/AppAuthTokenResponseUtil.java
@@ -1,31 +1,31 @@
 package com.comeon.backend.user.presentation.api;
 
-import com.comeon.backend.common.jwt.JwtClaims;
-import com.comeon.backend.common.jwt.JwtParser;
+import com.comeon.backend.common.jwt.JwtToken;
 import com.comeon.backend.user.application.Tokens;
 import com.comeon.backend.user.presentation.api.response.AppAuthTokensResponse;
 
 public class AppAuthTokenResponseUtil {
 
-    public static AppAuthTokensResponse generateResponse(JwtParser jwtParser, Tokens tokens) {
-        String accessTokenValue = tokens.getAccessToken().getToken();
-        JwtClaims accessTokenClaims = jwtParser.parse(accessTokenValue);
-        long accTokenExpiry = accessTokenClaims.getExpiration().getEpochSecond();
-        Long userId = accessTokenClaims.getUserId();
+    public static AppAuthTokensResponse generateResponse(Tokens tokens) {
+        JwtToken accessToken = tokens.getAccessToken();
+        AppAuthTokensResponse.AccessToken accessTokenResponse = null;
+        if (accessToken != null) {
+            accessTokenResponse = new AppAuthTokensResponse.AccessToken(
+                    accessToken.getToken(),
+                    accessToken.getClaims().getExpiration().getEpochSecond(),
+                    accessToken.getClaims().getUserId()
+            );
+        }
 
-        String refreshTokenValue = tokens.getRefreshToken().getToken();
-        long refTokenExpiry = jwtParser.parse(refreshTokenValue).getExpiration().getEpochSecond();
+        JwtToken refreshToken = tokens.getRefreshToken();
+        AppAuthTokensResponse.RefreshToken refreshTokenResponse = null;
+        if (refreshToken != null) {
+            refreshTokenResponse = new AppAuthTokensResponse.RefreshToken(
+                    refreshToken.getToken(),
+                    refreshToken.getClaims().getExpiration().getEpochSecond()
+            );
+        }
 
-        return new AppAuthTokensResponse(
-                new AppAuthTokensResponse.AccessToken(
-                        accessTokenValue,
-                        accTokenExpiry,
-                        userId
-                ),
-                new AppAuthTokensResponse.RefreshToken(
-                        refreshTokenValue,
-                        refTokenExpiry
-                )
-        );
+        return new AppAuthTokensResponse(accessTokenResponse, refreshTokenResponse);
     }
 }
