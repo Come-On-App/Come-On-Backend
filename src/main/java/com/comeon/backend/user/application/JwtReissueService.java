dcc6c9189384811ec7ecdbba0d9b942d21f32c67
hi0yoo <yoo971202@naver.com>
2023-01-16 11:14:59 +0900

    Refactor: 토큰 재발급 리팩토링
    내부 로직 코드 변경

--- src/main/java/com/comeon/backend/user/application/JwtReissueService.java
+++ src/main/java/com/comeon/backend/user/application/JwtReissueService.java
@@ -19,24 +19,26 @@ public class JwtReissueService {
     private final UserRepository userRepository;
 
     public Tokens reissue(String refreshTokenValue, boolean reissueRtkAlways) {
-        if (jwtValidator.valid(refreshTokenValue)) {
-            Long userId = refreshTokenRepository.getUserIdBy(refreshTokenValue).orElseThrow();
-            User user = userRepository.findById(userId).orElseThrow();
-
-            JwtToken accessToken = jwtGenerator.initBuilder(TokenType.ACCESS)
-                    .userId(user.getId())
-                    .authorities(user.getRole().getValue())
-                    .nickname(user.getNickname())
-                    .build();
-
-            // RTK 재발급 조건을 만족하거나 파라미터가 true 이면 RTK 재발급
-            JwtToken refreshToken = null;
-            if (jwtParser.isRtkSatisfiedAutoReissueCondition(refreshTokenValue) || reissueRtkAlways) {
-                refreshToken = jwtGenerator.initBuilder(TokenType.REFRESH).build();
-                refreshTokenRepository.add(refreshToken.getToken(), user.getId(), refreshToken.getClaims().getExpiration());
-            }
-            return new Tokens(accessToken, refreshToken);
+        if (!jwtValidator.valid(refreshTokenValue)) {
+            throw new RestApiException("리프레시 토큰 검증 실패", UserErrorCode.INVALID_USER_RTK);
         }
-        throw new RestApiException("리프레시 토큰 검증 실패", UserErrorCode.INVALID_USER_RTK);
+
+        Long userId = refreshTokenRepository.getUserIdBy(refreshTokenValue).orElseThrow();
+        User user = userRepository.findById(userId).orElseThrow();
+
+        JwtToken accessToken = jwtGenerator.initBuilder(TokenType.ACCESS)
+                .userId(user.getId())
+                .authorities(user.getRole().getValue())
+                .nickname(user.getNickname())
+                .build();
+
+        // RTK 재발급 조건을 만족하거나 파라미터가 true 이면 RTK 재발급
+        JwtToken refreshToken = null;
+        if (jwtParser.isRtkSatisfiedAutoReissueCondition(refreshTokenValue) || reissueRtkAlways) {
+            refreshToken = jwtGenerator.initBuilder(TokenType.REFRESH).build();
+            refreshTokenRepository.add(refreshToken.getToken(), user.getId(), refreshToken.getClaims().getExpiration());
+        }
+
+        return new Tokens(accessToken, refreshToken);
     }
 }
