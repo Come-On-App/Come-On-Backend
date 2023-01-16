6b9a56acf1ffd88efb804dc43a6792016a92addd
hi0yoo <yoo971202@naver.com>
2023-01-16 22:28:23 +0900

    Add: 모임 추가 API 작성
    presentation, application, domain layer 작성

---
+++ src/main/java/com/comeon/backend/meeting/domain/MeetingRepository.java
@@ -0,0 +1,6 @@
+package com.comeon.backend.meeting.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MeetingRepository extends JpaRepository<Meeting, Long> {
+}
