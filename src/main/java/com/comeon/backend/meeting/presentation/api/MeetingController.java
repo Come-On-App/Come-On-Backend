ee8ba01a13bd1c89c474c3b8ba48ccd920bcde03
hi0yoo <yoo971202@naver.com>
2023-01-20 21:20:44 +0900

    Add: 모임 리스트 조회 API

--- src/main/java/com/comeon/backend/meeting/presentation/api/MeetingController.java
+++ src/main/java/com/comeon/backend/meeting/presentation/api/MeetingController.java
@@ -1,18 +1,30 @@
 package com.comeon.backend.meeting.presentation.api;
 
+import com.comeon.backend.common.api.SliceResponse;
 import com.comeon.backend.common.security.JwtPrincipal;
 import com.comeon.backend.meeting.command.application.MeetingCommandService;
 import com.comeon.backend.meeting.command.application.MeetingMemberCommandService;
 import com.comeon.backend.meeting.command.application.MeetingMemberSummary;
 import com.comeon.backend.meeting.presentation.api.request.MeetingAddRequest;
 import com.comeon.backend.meeting.presentation.api.request.MeetingJoinRequest;
+import com.comeon.backend.meeting.presentation.api.request.MeetingSummaryListParam;
 import com.comeon.backend.meeting.presentation.api.response.MeetingAddResponse;
 import com.comeon.backend.meeting.presentation.api.response.MeetingJoinResponse;
+import com.comeon.backend.meeting.presentation.api.response.MeetingSummaryResponse;
+import com.comeon.backend.meeting.query.dao.MeetingDao;
+import com.comeon.backend.meeting.query.dao.MeetingCondition;
+import com.comeon.backend.meeting.query.dao.result.FindMeetingSliceResult;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
@@ -22,6 +34,7 @@ public class MeetingController {
 
     private final MeetingCommandService meetingCommandService;
     private final MeetingMemberCommandService meetingMemberCommandService;
+    private final MeetingDao meetingDao;
 
     @PostMapping
     public MeetingAddResponse meetingAdd(@AuthenticationPrincipal JwtPrincipal jwtPrincipal,
@@ -37,12 +50,47 @@ public class MeetingController {
         return new MeetingAddResponse(meetingId);
     }
 
+    @GetMapping
+    public SliceResponse<MeetingSummaryResponse> meetingSummaryList(@AuthenticationPrincipal JwtPrincipal jwtPrincipal,
+                                                                    @PageableDefault(size = 10, page = 0) Pageable pageable,
+                                                                    MeetingSummaryListParam param) {
+        // TODO 리팩토링
+        Slice<FindMeetingSliceResult> meetingSlice = meetingDao.findMeetingSlice(
+                jwtPrincipal.getUserId(),
+                pageable,
+                new MeetingCondition(
+                        param.getMeetingName(),
+                        param.getDateFrom(),
+                        param.getDateTo()
+                )
+        );
+
+        return SliceResponse.toSliceResponse(
+                meetingSlice.map(
+                        result -> new MeetingSummaryResponse(
+                                result.getMeetingId(),
+                                new MeetingSummaryResponse.UserSummaryResponse(
+                                        result.getHostUserId(),
+                                        result.getHostUserNickname(),
+                                        result.getHostUserProfileImageUrl()
+                                ),
+                                result.getMemberCount(),
+                                result.getMyMeetingRole(),
+                                result.getMeetingName(),
+                                LocalDate.parse(result.getCalendarStartFrom(), DateTimeFormatter.ofPattern("yyyy-MM-dd")),
+                                LocalDate.parse(result.getCalendarEndTo(), DateTimeFormatter.ofPattern("yyyy-MM-dd")),
+                                result.getMeetingImageUrl(),
+                                null,
+                                null
+                        )
+                )
+        );
+    }
+
     @PostMapping("/join")
     public MeetingJoinResponse meetingJoin(@AuthenticationPrincipal JwtPrincipal jwtPrincipal,
                                            @Validated @RequestBody MeetingJoinRequest request) {
         MeetingMemberSummary meetingMemberSummary = meetingMemberCommandService.joinMeeting(jwtPrincipal.getUserId(), request.getEntryCode());
         return new MeetingJoinResponse(meetingMemberSummary.getMeetingId(), meetingMemberSummary.getMemberId(), meetingMemberSummary.getMemberRole());
     }
-
-
 }
---
+++ src/main/java/com/comeon/backend/meeting/presentation/api/request/MeetingSummaryListParam.java
@@ -0,0 +1,23 @@
+package com.comeon.backend.meeting.presentation.api.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MeetingSummaryListParam {
+
+    private String meetingName;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dateFrom;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dateTo;
+}
