4b701c5001e64ef490c0344c09d586206ce4950b
hi0yoo <yoo971202@naver.com>
2023-01-30 12:35:15 +0900

    Modify: 인증 실패시 응답 메시지 분리
    토큰 없음 : 토큰 없다는 오류 메시지
    토큰은 있으나 검증 실패 : 유효하지 않은 토큰 메시지

--- src/main/java/com/comeon/backend/common/error/CommonErrorCode.java
+++ src/main/java/com/comeon/backend/common/error/CommonErrorCode.java
@@ -13,8 +13,11 @@ public enum CommonErrorCode implements ErrorCode {
     REQUEST_VALIDATION_FAIL(1002, HttpStatus.BAD_REQUEST, "요청 데이터 검증에 실패하였습니다. API 스펙을 확인해주세요."),
     HTTP_METHOD_NOT_SUPPORTED(1003, HttpStatus.METHOD_NOT_ALLOWED, "해당 HTTP METHOD는 지원하지 않습니다. API 스펙을 확인해주세요."),
     BIND_ERROR(1004, HttpStatus.BAD_REQUEST, "파라미터로 입력된 데이터 형식이 잘못되었습니다."),
+
     UNAUTHORIZED(1100, HttpStatus.UNAUTHORIZED, "인증된 사용자만이 이용할 수 있습니다."),
     NO_AUTHORITIES(1101, HttpStatus.FORBIDDEN, "해당 요청에 필요한 권한이 부족합니다."),
+    NO_AUTHORIZATION_HEADER(1102, HttpStatus.UNAUTHORIZED, "인증 헤더를 찾을 수 없습니다."),
+    INVALID_AUTHORIZATION_HEADER(1103, HttpStatus.UNAUTHORIZED, "엑세스 토큰이 유효하지 않습니다."),
     ;
 
     private final int code;
--- src/main/java/com/comeon/backend/common/security/JwtAuthenticationFilter.java
+++ src/main/java/com/comeon/backend/common/security/JwtAuthenticationFilter.java
@@ -3,7 +3,6 @@ package com.comeon.backend.common.security;
 import com.comeon.backend.jwt.application.JwtManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.util.StringUtils;
@@ -19,8 +18,6 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private final static String PREFIX_BEARER = "Bearer ";
-
     private final JwtManager jwtManager;
     private final JwtAuthenticationProvider authenticationProvider;
 
@@ -28,7 +25,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        String accessTokenValue = resolveAccessToken(request);
+        String accessTokenValue = AuthHeaderParser.resolveAccessToken(request);
 
         if (isValidAccessToken(accessTokenValue)) {
             Authentication authentication = authenticationProvider.getAuthentication(accessTokenValue);
@@ -40,13 +37,5 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private boolean isValidAccessToken(String accessTokenValue) {
         return StringUtils.hasText(accessTokenValue) && jwtManager.verifyAtk(accessTokenValue);
-    }
-
-    private String resolveAccessToken(HttpServletRequest request) {
-        String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-        if (StringUtils.hasText(authorizationHeader) && authorizationHeader.startsWith(PREFIX_BEARER)) {
-            return authorizationHeader.substring(PREFIX_BEARER.length());
-        }
-        return null;
     }
 }
