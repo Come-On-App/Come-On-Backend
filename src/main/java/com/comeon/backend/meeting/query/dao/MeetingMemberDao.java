4d7714f7c3a7c2b56295806063eb057009c2267c
hi0yoo <yoo971202@naver.com>
2023-01-31 12:32:45 +0900

    Add: 모임 회원 리스트 조회 API 추가
    컨트롤러 API 작성
    매퍼 작성

--- src/main/java/com/comeon/backend/meeting/query/dao/MeetingMemberDao.java
+++ src/main/java/com/comeon/backend/meeting/query/dao/MeetingMemberDao.java
@@ -1,8 +1,13 @@
 package com.comeon.backend.meeting.query.dao;
 
+import com.comeon.backend.meeting.query.dto.MemberListResponse;
 import com.comeon.backend.meeting.query.dto.MemberSimpleResponse;
+
+import java.util.List;
 
 public interface MeetingMemberDao {
 
     MemberSimpleResponse findMemberSimple(Long meetingId, Long userId);
+
+    List<MemberListResponse> findMemberList(Long meetingId);
 }
