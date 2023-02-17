e7868af8c066d40f67ea66d2cd05f473ddba4019
hi0yoo <yoo971202@naver.com>
2023-02-17 15:35:59 +0900

    Modify: 모임일 리스트 조회 응답 변경

--- src/main/java/com/comeon/backend/date/query/dto/MeetingFixedDateSummary.java
+++ src/main/java/com/comeon/backend/date/query/dto/MeetingFixedDateSummary.java
@@ -1,8 +1,11 @@
 package com.comeon.backend.date.query.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
 
 @Getter
 @NoArgsConstructor
@@ -12,4 +15,7 @@ public class MeetingFixedDateSummary {
     private Long meetingId;
     private String meetingName;
     private FixedDateSimple fixedDate;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
+    private LocalTime meetingStartTime;
 }
--- src/main/java/com/comeon/backend/date/query/dto/MeetingFixedDatesResponse.java
+++ src/main/java/com/comeon/backend/date/query/dto/MeetingFixedDatesResponse.java
@@ -2,33 +2,28 @@ package com.comeon.backend.date.query.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 public class MeetingFixedDatesResponse {
 
-    private List<YearMeetings> result = new ArrayList<>();
+    private List<YearMeetings> result;
 
     @Getter
-    @NoArgsConstructor
     @AllArgsConstructor
     public static class YearMeetings {
 
         private Integer year;
-        private List<MonthCalendar> calendar = new ArrayList<>();
+        private List<MonthCalendar> calendar;
     }
 
     @Getter
-    @NoArgsConstructor
     @AllArgsConstructor
     public static class MonthCalendar {
 
         private Integer month;
-        private List<MeetingFixedDateSummary> meetings = new ArrayList<>();
+        private List<MeetingFixedDateSummary> meetings;
     }
 }
