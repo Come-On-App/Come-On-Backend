464d05f869dee880c7aec57947853817bbbdb2b1
hi0yoo <yoo971202@naver.com>
2023-03-25 21:17:28 +0900

    add: kakao login test

--- src/main/java/com/comeon/backend/user/presentation/web/OauthPageController.java
+++ src/main/java/com/comeon/backend/user/presentation/web/OauthPageController.java
@@ -12,7 +12,8 @@ public class OauthPageController {
     @GetMapping("/oauth/callback/kakao")
     public String kakaoOauthPage(@RequestParam String code) {
         log.debug("code: {}", code);
-        String redirectUrl = "exp://127.0.0.1:19000/oauth/callback/kakao?code=" + code;
+//        String redirectUrl = "exp://127.0.0.1:19000/oauth/callback/kakao?code=" + code;
+        String redirectUrl = "exp://192.168.1.3:19000/oauth/callback/kakao?code=" + code;
         log.debug("redirectUrl: {}", redirectUrl);
 
         return "redirect:" + redirectUrl;
