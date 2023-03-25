cdad3bfe8e443ad3b6326146d18105248b4c2468
hi0yoo <yoo971202@naver.com>
2023-03-25 19:58:38 +0900

    add: kakao login test

--- src/main/java/com/comeon/backend/user/presentation/web/OauthPageController.java
+++ src/main/java/com/comeon/backend/user/presentation/web/OauthPageController.java
@@ -4,24 +4,22 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Slf4j
 @Controller
 public class OauthPageController {
 
     @GetMapping("/oauth/callback/kakao")
-    public String kakaoOauthPage(HttpServletResponse response) {
-        ResponseCookie responseCookie = ResponseCookie.from("_kawit", "")
-                .maxAge(0)
-                .domain(".kakao.com")
-                .path("/")
-                .secure(true)
-                .sameSite("None")
-                .build();
-        response.addHeader("Set-Cookie", responseCookie.toString());
+    public String kakaoOauthPage(@RequestParam String code,
+                                 HttpServletResponse response) throws IOException {
+        log.debug("code: {}", code);
+        String redirectUrl = "come-on://oauth/callback/kakao?code=" + code;
+        log.debug("redirectUrl: {}", redirectUrl);
 
-        return "oauth-callback";
+        return "redirect:" + redirectUrl;
     }
 }
